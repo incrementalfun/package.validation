e2b2e682d7ef587b270945bb0892723a971bde66
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-07-07 20:34:14 +0200

    feat: implement default validators

---
+++ src/Incremental.Common.Validation/Validators.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using FluentValidation;
+using Humanizer;
+using Microsoft.EntityFrameworkCore;
+
+namespace Incremental.Common.Validation
+{
+    public static class Validators
+    {
+        public static IRuleBuilderOptions<T, Guid> EntityMustExist<T, TEntity>(this IRuleBuilder<T, Guid> ruleBuilder, DbContext dbContext) where TEntity : class
+        {
+            return ruleBuilder.MustAsync(async (id, cancellationToken) =>
+            {
+                return await dbContext.Set<TEntity>().FindAsync(new object[] {id}, cancellationToken) is not null;
+            }).WithMessage(nameof(EntityMustExist).Humanize(LetterCasing.Sentence));
+        }
+        
+        public static IRuleBuilderOptions<T, Guid> EntityMustNotExist<T, TEntity>(this IRuleBuilder<T, Guid> ruleBuilder, DbContext dbContext) where TEntity : class
+        {
+            return ruleBuilder.MustAsync(async (id, cancellationToken) =>
+            {
+                return await dbContext.Set<TEntity>().FindAsync(new object[] {id}, cancellationToken) is null;
+            }).WithMessage(nameof(EntityMustNotExist).Humanize(LetterCasing.Sentence));
+        }
+    }
+}
