ef4d40ec8c2b81354e2c57a1dc92ee027355d354
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-07-07 20:42:00 +0200

    feat: add extension method for DI

---
+++ src/Incremental.Common.Validation/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+using System.Reflection;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Incremental.Common.Validation
+{
+    /// <summary>
+    /// Extensions for adding validation resources.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds validation pipelines and services to the application.
+        /// </summary>
+        /// <param name="mvcBuilder"><see cref="IMvcBuilder"/>.</param>
+        /// <param name="assemblies">Assembly or assemblies containing validators.</param>
+        /// <returns></returns>
+        public static IMvcBuilder AddCommonValidation(this IMvcBuilder mvcBuilder, params Assembly[] assemblies)
+        {
+            return mvcBuilder.AddFluentValidation(
+                options =>
+                {
+                    options.RegisterValidatorsFromAssemblies(assemblies);
+                });
+        }
+
+    }
+}
